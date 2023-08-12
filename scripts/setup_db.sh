dca12ad7e0f8a4c0f9dfb083fceec8769aca6e12
Uzair Aftab <uzaaft@outlook.com>
2023-08-12 14:55:44 +0200

    feat: Add setup script to setup db and env

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -7,4 +7,4 @@ port=5436
 
 docker container inspect $name >/dev/null 2>&1 || docker run -d --restart unless-stopped --name $name -p $port:5432 -e POSTGRES_PASSWORD=password postgres:latest >/dev/null
 
-echo 'export DATABASE_URL="postgresql://postgres:password@localhost:'$port'/postgres"'
+echo 'export DATABASE_URL="postgresql://postgres:password@localhost:'$port'/postgres"' >>.env
